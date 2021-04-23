15ad27519b047a83426c54ce16b01b0574483b79
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-04-23 19:12:32 -0400

    started product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,18 +4,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
+router.get('/', async (req, res) => {
+  // WORKS find all products
   // be sure to include its associated Category and Tag data
+  const products = await Product.findAll({
+    include: [{ model: Tag, through: ProductTag, as: 'tagged_products' }]
+  })
+  console.log(products);
+  res.json(products)
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
+  //WORKS find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [{ model: Tag, through: ProductTag, as: 'tagged_products' }]
+  }).then((productData) => {
+
+    res.json(productData);
+  });
 });
 
-// create new product
+//TODO: do we need to fix this at all??
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
@@ -89,8 +100,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  //TODO: NOT WORKING delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
