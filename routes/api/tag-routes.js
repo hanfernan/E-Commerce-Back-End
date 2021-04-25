767b0a5e542ca26ea3f74f69524cfd8b9ebf286c
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-04-25 16:01:13 -0400

    fixed post request for tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,9 +30,10 @@ router.get('/:id', (req, res) => {
 router.post('/', async (req, res) => {
   // WORKS: create a new tag
   try {
-    const tagData = await Tag.create({
-      tag_name: req.body.tag_name
-    });
+    const tagData = await Tag.create(
+      // {tag_name: req.body.tag_name}
+      req.body
+    );
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
